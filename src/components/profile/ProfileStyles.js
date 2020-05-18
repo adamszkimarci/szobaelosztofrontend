977759526b00c0464518c175a610503018531300
hmart1997 <harangozom97@gmail.com>
2020-05-19 00:39:05 +0200

    Profile added to components

---
+++ src/components/profile/ProfileStyles.js
@@ -0,0 +1,15 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+const profileStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    profile: {
+        marginTop: theme.spacing(8),
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+export default profileStyles;
